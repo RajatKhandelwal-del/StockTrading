94b2bb3eddfe4b26689cb0b856f7374176ceee71
RajatKhandelwal <rajatkhandelwal2310@gmail.com>
2026-04-13 14:14:33 +0530

    First Commit

---
+++ frontend/src/landing_page/products/Hero.jsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <div className="container border-bottom">
+      <div className="text-center mt-5 p-3">
+        <h1>Technology</h1>
+        <h5 className="text-muted mt-3 fs-4">
+          Sleek, modern, intuitive trading platforms
+        </h5>
+        <p className="mb-5 mt-3">Check out our investment offerings</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,27 @@
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import HomePage from './landing_page/home/HomePage.jsx'
+import AboutPage from './landing_page/about/AboutPage.jsx'
+import PricingPage from './landing_page/pricing/PricingPage.jsx'
+import ProductsPage from './landing_page/products/ProductsPage.jsx'
+import SignUp from './landing_page/signup/SignUp.jsx'
+import SupportPage from './landing_page/support/SupportPage.jsx'
+import Navbar from './landing_page/Navbar.jsx'
+import Footer from './landing_page/Footer.jsx'
+import NotFound from './landing_page/NotFound.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/about" element={<AboutPage/>}></Route>
+      <Route path="/product" element={<ProductsPage/>}></Route>
+      <Route path="/pricing" element={<PricingPage/>}></Route>
+      <Route path="/support" element={<SupportPage/>}></Route>
+      <Route path="*" element={<NotFound/>}></Route>
+    </Routes>
+    <Footer/>
+  </BrowserRouter>
+)
